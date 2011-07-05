8ee11d397d76fa2a752d82bf6fabc84ce2ac403b
Tal Yalon <tal.yalon@gmail.com>
2011-07-05 14:14:48 +0300

    removed the api_example_case - it's msim specific

--- doc/conf.py
+++ doc/conf.py
@@ -26,7 +26,7 @@ import sys, os
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 sys.path.insert(0, os.path.dirname(__file__))
-extensions = ['api_example_case']
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

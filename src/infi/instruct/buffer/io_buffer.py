994ee60b5ea39e08664aac07c6f17769c6142638
Tal Yalon <tal.yalon@gmail.com>
2012-06-07 18:00:02 +0300

    added bit aware buffers

--- src/infi/instruct/buffer/io_buffer.py
+++ src/infi/instruct/buffer/io_buffer.py
@@ -1,3 +1,230 @@
+import math
+import collections
+
+def float_to_byte_bit_pair(n):
+    return (int(n), int((n - int(n)) * 8))
+
+class BitView(collections.Sequence):
+    """
+    Provides bit indexing over an existing buffer.
+
+    For example, you can do this:
+    bit_view = BitView(...)
+    bits_2_and_3 = bit_view[0.125 * 2:0.125 * 4]
+
+    Basically, you can access the buffer with 1/8 fractions.
+    """
+
+    def __init__(self, buffer, start=0, stop=None):
+        super(BitView, self).__init__()
+        self.buffer = buffer
+        self.start = start if start is not None else 0
+        self.stop = stop if stop is not None else len(buffer)
+        assert self.start >= 0
+        assert self.stop <= len(buffer)
+        assert self.start <= self.stop
+
+    def __getitem__(self, key):
+        start, stop = self._key_to_range(key)
+        if isinstance(key, slice):
+            return self._get_range(start, stop)
+        else: # must be int/float otherwise _key_to_range would raise an error
+            return self._get_byte_bits(start, 8)
+
+    def __len__(self):
+        return int(math.ceil(self.stop - self.start))
+
+    def __iter__(self):
+        for i in xrange(len(self)):
+            yield self._get_byte_bits(i + self.start, min(int((self.stop - i - self.start) * 8), 8))
+
+    def __str__(self):
+        return str(bytearray(b for b in self))
+
+    def __repr__(self):
+        return "{0}(buffer={1!r}, start={2}, stop={3})".format(type(self), self.buffer, self.start, self.stop)
+
+    def length(self):
+        return self.stop - self.start
+
+    def _get_range(self, start, stop):
+        start_byte_ofs = int(start)
+        stop_byte_ofs = int(math.ceil(stop))
+        return type(self)(self.buffer[start_byte_ofs:stop_byte_ofs], start - start_byte_ofs, stop - start_byte_ofs)
+
+    def _get_byte_bits(self, ofs, bit_len):
+        byte_ofs, bit_ofs = float_to_byte_bit_pair(ofs)
+        bit_mask = ((1 << bit_len) - 1)
+        if bit_ofs == 0:
+            return self.buffer[byte_ofs] & bit_mask
+        elif bit_ofs + bit_len <= 8:
+            return (self.buffer[byte_ofs] >> bit_ofs) & bit_mask
+        else:
+            next_byte = self.buffer[byte_ofs + 1] if byte_ofs + 1 < len(self.buffer) else 0
+            return (((self.buffer[byte_ofs] >> bit_ofs) & 0xFF) | ((next_byte << (8 - bit_ofs)) & 0xFF)) &  bit_mask
+
+    def _key_to_range(self, key):
+        if isinstance(key, slice):
+            if key.step not in (None, 1):
+                raise NotImplementedError("step must be 1 or None")
+            start = self._translate_offset(key.start if key.start is not None else 0)
+            stop = self._translate_offset(key.stop if key.stop is not None else self.length())
+        elif isinstance(key, (float, int)):
+            start = self._translate_offset(key)
+            stop = start + 1
+        else:
+            raise TypeError("index must be int, float or a slice")
+        return start, stop
+
+    def _translate_offset(self, ofs):
+        length = self.length()
+        ofs = max(ofs + length, 0) if ofs < 0 else min(ofs, length)
+        return ofs + self.start
+
+class BitAwareByteArray(BitView, collections.MutableSequence):
+    """
+    Similar to BitView, but this class is mutable.
+    """
+    def __init__(self, source, start=0, stop=None):
+        assert isinstance(source, bytearray)
+        super(BitAwareByteArray, self).__init__(source, start, stop)
+
+    def __setitem__(self, key, value):
+        start, stop = self._key_to_range(key)
+        value, value_len = self._value_to_value_and_length(value, stop - start)
+        self._set_range(start, stop, value, value_len)
+
+    def __delitem__(self, key):
+        start, stop = self._key_to_range(key)
+        self._del_range(start, stop)
+
+    def insert(self, i, value):
+        i = self._translate_offset(i)
+        value, value_len = self._value_to_value_and_length(value)
+        self._insert_zeros(i, i + value_len)
+        self._copy_to_range(i, value, value_len)
+
+    def extend(self, other):
+        if not isinstance(other, BitView):
+            return super(BitAwareByteArray, self).extend(other)
+        offset = self.stop
+        self._insert_zeros(offset, offset + other.length())
+        self._copy_to_range(offset, other, other.length())
+
+    def __add__(self, other):
+        if not isinstance(other, BitView):
+            return NotImplemented
+        copy = BitAwareByteArray(bytearray(self.buffer), start=self.start, stop=self.stop)
+        copy.extend(other)
+        return copy
+
+    def __radd__(self, other):
+        if not isinstance(other, BitView):
+            return NotImplemented
+
+        copy = BitAwareByteArray(bytearray(other.buffer), start=other.start, stop=other.stop)
+        copy.extend(self)
+        return copy
+
+    def _set_range(self, start, stop, value, value_len):
+        """
+        Assumes that start and stop are already in 'buffer' coordinates. value is a byte iterable.
+        value_len is fractional.
+        """
+        assert stop >= start and value_len >= 0
+        range_len = stop - start
+        if range_len < value_len:
+            self._insert_zeros(stop, stop + value_len - range_len)
+            self._copy_to_range(start, value, value_len)
+        elif range_len > value_len:
+            self._del_range(stop - (range_len - value_len), stop)
+            self._copy_to_range(start, value, value_len)
+        else:
+            self._copy_to_range(start, value, value_len)
+
+    def _copy_to_range(self, offset, iterable, iterable_len):
+        remaining_bit_len = int(iterable_len * 8)
+        for byte in iterable:
+            self._set_byte_bits(offset, min(remaining_bit_len, 8), byte)
+            offset += 1
+            remaining_bit_len -= 8
+
+    def _del_range(self, start, stop):
+        assert stop >= start
+        start_byte, stop_byte = int(math.ceil(start)), int(math.floor(stop))
+        whole_byte_delta = stop_byte - start_byte
+
+        # If we can remove whole bytes from the buffer, we'll do that first.
+        if whole_byte_delta >= 1:
+            del self.buffer[start_byte:stop_byte]
+            self.stop -= whole_byte_delta
+            stop -= whole_byte_delta
+
+        # Here we have at most 8 bits to remove, so we need to "shift" the entire array.
+        if stop > start:
+            ofs = start
+            bit_len_frac = stop - start
+            while (ofs + bit_len_frac) < self.stop:
+                self._set_byte_bits(ofs, 8, self._get_byte_bits(ofs + bit_len_frac, 8))
+                ofs += 1
+            self.stop -= bit_len_frac
+            if int(math.ceil(self.stop)) < len(self.buffer):
+                del self.buffer[-1]
+
+    def _insert_zeros(self, start, stop):
+        assert stop >= start
+        start_byte, stop_byte = int(math.ceil(start)), int(math.floor(stop))
+        whole_byte_delta = stop_byte - start_byte
+
+        # If we can insert whole bytes to the buffer, we'll do that first.
+        if whole_byte_delta >= 1:
+            self.buffer[start_byte:start_byte] = bytearray(whole_byte_delta)
+            self.stop += whole_byte_delta
+            stop -= whole_byte_delta
+
+        if stop > start:
+            bit_len_frac = stop - start
+            if int(math.ceil(self.stop + bit_len_frac)) > len(self.buffer):
+                self.buffer.append(0)
+            ofs = self.stop + bit_len_frac - 1
+            while ofs >= start:
+                self._set_byte_bits(ofs, 8, self._get_byte_bits(ofs - bit_len_frac, 8))
+                ofs -= 1
+            self.stop += bit_len_frac
+
+    def _set_byte_bits(self, ofs, bit_len, value):
+        byte_ofs, bit_ofs = float_to_byte_bit_pair(ofs)
+        if bit_ofs == 0 and bit_len == 8:
+            self.buffer[byte_ofs] = value # shortcut
+        elif (bit_ofs + bit_len) <= 8:
+            self.buffer[byte_ofs] &= ~(((1 << bit_len) - 1) << bit_ofs)
+            self.buffer[byte_ofs] |= (value << bit_ofs) & 0xFF
+        else:
+            first_byte_bit_len = 8 - bit_ofs
+            self._set_byte_bits(ofs, first_byte_bit_len, value & ((1 << first_byte_bit_len) - 1))
+            self._set_byte_bits(byte_ofs + 1, bit_len - first_byte_bit_len, value >> first_byte_bit_len)
+
+    def _value_to_value_and_length(self, value, int_value_len=1):
+        value_len = 0
+        if isinstance(value, BitView):
+            value_len = value.length()
+        elif isinstance(value, collections.Sized):
+            value_len = len(value)
+        elif isinstance(value, collections.Iterable):
+            value = bytearray(value)
+            value_len = len(value)
+        elif isinstance(value, int):
+            # Short circuit: make bit ranges accept int values if their len <= 8
+            value_len = min(1, int_value_len)
+            if value.bit_length() > value_len * 8:
+                # Safety measure for short circuit: if user is assigning an int with more bits than the range of
+                # bits that he specified we shout.
+                raise ValueError("trying to assign int {0} to bit length {1}".format(value, value_len))
+            value = [ value ]
+        else:
+            raise TypeError("value must be iterable or int")
+        return value, value_len
+
 class InputBuffer(object):
     def __init__(self, buffer):
         self.buffer = buffer
---
+++ tests/test_buffer_io.py
@@ -0,0 +1,190 @@
+import random
+from bitarray import bitarray
+from infi.unittest import TestCase
+from infi.instruct.buffer.io_buffer import BitView, BitAwareByteArray
+
+random.seed(0)
+
+class IOBufferTestCase(TestCase):
+    def test_getitem__byte(self):
+        buf = BitAwareByteArray(bytearray((1, 2, 4)), 0, 3)
+        self.assertEqual(1, buf[0])
+        self.assertEqual(2, buf[1])
+        self.assertEqual(1, buf[1.125])
+        self.assertEqual(4, buf[2])
+        self.assertEqual(2, buf[2.125])
+        self.assertEqual(1, buf[2.25])
+        self.assertEqual(0, buf[2.375])
+
+        buf = BitAwareByteArray(bytearray((128, 1)), 0, 2)
+        self.assertEqual(3, buf[1 - 0.125])
+
+        buf = BitAwareByteArray(bytearray((2, 4)), 0.125, 2)
+        self.assertEqual(1, buf[0])
+        self.assertEqual(2, buf[1])
+        self.assertEqual(1, buf[1.125])
+
+    def test_getitem__range(self):
+        buf = BitAwareByteArray(bytearray((1, 2, 4, 128, 1)), 0, 5)
+        self.assertEqual([ 1 ], list(buf[0:1]))
+        self.assertEqual([ 1, 2 ], list(buf[0:2]))
+        self.assertEqual([ 2, 4 ], list(buf[1:3]))
+        self.assertEqual([ 2, 4, 128, 1 ], list(buf[1:]))
+        self.assertEqual([ 1, 2 ], list(buf[:2]))
+        self.assertEqual([ 0, 1 ], list(buf[0.125:2.125]))
+        self.assertEqual([ 0, 1 ], list(buf[0.125:2.125]))
+        self.assertEqual([ 128, 1], list(buf[-2:]))
+        self.assertEqual([ 1 ], list(buf[-10:-4]))
+        self.assertEqual([ ], list(buf[-10:-5]))
+
+    def test_setitem__byte(self):
+        buf = BitAwareByteArray(bytearray((1, 2, 4)), 0, 3)
+        buf[0:1] = 3
+        self.assertEqual([ 3, 2, 4 ], list(buf))
+
+        buf = BitAwareByteArray(bytearray((1, 2, 4)), 0, 3)
+        buf[0.125:1.125] = 3
+        self.assertEqual([ 7, 2, 4 ], list(buf))
+
+        buf = BitAwareByteArray(bytearray((1, 2, 4)), 0, 3)
+        buf[0.125:1.125] = 0x83
+        self.assertEqual([ 7, 3, 4 ], list(buf))
+
+    def test_setitem__bits(self):
+        buf = BitAwareByteArray(bytearray((1, 2, 4)), 0, 3)
+        buf[0:0.125] = 0
+        self.assertEqual([ 0, 2, 4 ], list(buf))
+
+        buf = BitAwareByteArray(bytearray((1, 2, 4)), 0, 3)
+        buf[0.125:0.25] = 1
+        self.assertEqual([ 3, 2, 4 ], list(buf))
+
+        buf = BitAwareByteArray(bytearray((1, 2, 4)), 0, 3)
+        buf[1.125:1.375] = 3
+        self.assertEqual([ 1, 6, 4 ], list(buf))
+
+    def test_setitem__insert_into_empty_range(self):
+        buf = BitAwareByteArray(bytearray((1, 2, 4)), 0, 3)
+        buf[0.125:0.125] = BitView(bytearray((1,)), 0, 0.125)
+        self.assertEqual([ 3, 4, 8, 0 ], list(buf))
+
+        buf = BitAwareByteArray(bytearray((1, 2, 4)), 0, 3)
+        buf[0:0] = BitView(bytearray((1,)), 0, 0.125)
+        self.assertEqual([ 3, 4, 8, 0 ], list(buf))
+
+        buf = BitAwareByteArray(bytearray((1, 2, 4)), 0, 3)
+        buf[0.25:0.25] = BitView(bytearray(1), 0, 0.125)
+        self.assertEqual([ 1, 4, 8, 0 ], list(buf))
+
+        buf = BitAwareByteArray(bytearray((1, 2, 4)), 0, 3)
+        buf[0.25:0.25] = BitView(bytearray((1,)), 0, 0.125)
+        self.assertEqual([ 5, 4, 8, 0 ], list(buf))
+
+    def test_setitem__smaller_val(self):
+        ba = bitarray('1001010111', endian='little')
+        bv = BitAwareByteArray(self._bitarray_to_bytes(ba), stop=float(ba.length()) / 8)
+        val = bitarray('10', endian='little')
+        ba[3:7] = val
+        bv[3.0 / 8:7.0 / 8] = BitView(self._bitarray_to_bytes(val), stop=2.0 / 8)
+        self.assertEqualBitArrayBitView(ba, bv)
+
+    def test_delitem__bits(self):
+        buf = BitAwareByteArray(bytearray((1, 2, 4)))
+        del buf[0:1]
+        self.assertEqual([ 2, 4 ], list(buf))
+
+        buf = BitAwareByteArray(bytearray((1, 2, 4)))
+        del buf[1:]
+        self.assertEqual([ 1 ], list(buf))
+
+        buf = BitAwareByteArray(bytearray((1, 2, 4)))
+        del buf[0:0.125]
+        self.assertEqual([ 0, 1, 2 ], list(buf))
+
+        buf = BitAwareByteArray(bytearray((1, 2, 4)))
+        del buf[0:1.125]
+        self.assertEqual([ 1, 2 ], list(buf))
+
+        buf = BitAwareByteArray(bytearray((1, 2, 4)))
+        del buf[0:2.25]
+        self.assertEqual([ 1 ], list(buf))
+
+    def test_insert__bytes(self):
+        buf = BitAwareByteArray(bytearray((1, 2, 4)))
+        buf.insert(3, bytearray((8, 16)))
+        self.assertEqual([ 1, 2, 4, 8, 16 ], list(buf))
+
+        buf = BitAwareByteArray(bytearray((1, 2, 4)))
+        # 100000000 01000000 00100000
+        buf.insert(1.25, bytearray((8, 16)))
+        #   100000000 01 00010000 00001000 000000 00100000
+        # = 100000000 01000100 00000010 00000000 00100000
+        # = 1         34       64       0        4
+        self.assertEqual([ 1, 34, 64, 0, 4 ], list(buf))
+
+    def test_extend(self):
+        buf = BitAwareByteArray(bytearray((1, 2, 3)))
+        buf.extend(bytearray((4, 5)))
+        self.assertEqual([ 1, 2, 3, 4, 5 ], list(buf))
+
+    def test_bitview_getitem__single_byte_bitslice(self):
+        for i in xrange(0, 256):
+            for j in xrange(0, 8):
+                bv = BitView(bytearray([ i ]))
+                self.assertEqual(list(bv[float(j) / 8:])[0], i >> j)
+
+    def test_bitview_getitem__single_byte_bitslice_with_bits(self):
+        for i in xrange(0, 256):
+            for j in xrange(0, 8):
+                bv = BitView(bytearray([ i ]))
+                bv_slice = bv[float(j) / 8:]
+                ba = bitarray(endian='little')
+                ba.frombytes(chr(i))
+                ba_slice = ba[j:]
+                self.assertEqualBitArrayBitView(ba_slice, bv_slice)
+
+    def test_bitview__positive_slicing(self):
+        for i in xrange(0, 100):
+            ba = self._create_random_bit_array()
+            bv = BitView(self._bitarray_to_bytes(ba), stop=float(ba.length()) / 8)
+            self.assertEqualBitArrayBitView(ba, bv)
+
+            slice_start_in_bits = random.choice(range(0, ba.length() + 10))
+            slice_end_in_bits = random.choice(range(slice_start_in_bits, ba.length() + 10))
+
+            ba_slice = ba[slice_start_in_bits:slice_end_in_bits]
+            bv_slice = bv[float(slice_start_in_bits) / 8:float(slice_end_in_bits) / 8]
+            self.assertEqualBitArrayBitView(ba_slice, bv_slice)
+
+    def test_add(self):
+            ba1 = self._create_random_bit_array()
+            ba2 = self._create_random_bit_array()
+            ba = ba1 + ba2
+            bv1 = BitAwareByteArray(self._bitarray_to_bytes(ba1), stop=float(ba1.length()) / 8)
+            bv2 = BitAwareByteArray(self._bitarray_to_bytes(ba2), stop=float(ba2.length()) / 8)
+            bv = bv1 + bv2
+            self.assertEqualBitArrayBitView(ba, bv)
+
+    def test_radd(self):
+            ba1 = self._create_random_bit_array()
+            ba2 = self._create_random_bit_array()
+            ba = ba1 + ba2
+            bv1 = BitView(self._bitarray_to_bytes(ba1), stop=float(ba1.length()) / 8)
+            bv2 = BitAwareByteArray(self._bitarray_to_bytes(ba2), stop=float(ba2.length()) / 8)
+            bv = bv1 + bv2
+            self.assertEqualBitArrayBitView(ba, bv)
+
+    def assertEqualBitArrayBitView(self, ba, bv):
+        self.assertEqual(ba.length(), 8 * bv.length())
+        ba_bytes = self._bitarray_to_bytes(ba)
+        bv_bytes = str(bv)
+        self.assertEqual(ba_bytes, bv_bytes)
+
+    def _create_random_bit_array(self):
+        length_in_bits = random.randint(0, 8 * 16)
+        return bitarray("".join(random.choice(('0', '1')) for i in xrange(length_in_bits)), endian='little')
+
+    def _bitarray_to_bytes(self, b):
+        copy = bitarray(b, endian='little')
+        copy.fill()
+        return bytearray(copy.tobytes())
